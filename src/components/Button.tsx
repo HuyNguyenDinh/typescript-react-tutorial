5d09829f62fc59a45ae997708847d79f5d761545
huynd86 <huynd86@fpt.com.vn>
2023-01-16 17:42:55 +0700

    initial

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+    handleClick: (evt: React.MouseEvent<HTMLButtonElement>, id: number,) => void,
+    
+}
+
+export const Button = (props: ButtonProps) => {
+    return(
+        <button onClick={e => props.handleClick(e, 1)}>Click</button>
+    )
+}
